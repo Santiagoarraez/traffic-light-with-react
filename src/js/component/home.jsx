bb38ce46dca48a315b767cb0bb075386063a3feb
Santiagoarraez <Santiagoarraez@gmail.com>
2023-03-20 00:03:22 +0000

    ADD traffic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import "../../styles/index.css"
 
 //create your first component
 const Home = () => {
+	const [lightUp, setLightUp] = useState("danger")
+	const handleClick = (color) => {
+		setLightUp(color);
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="flex-column d-flex align-items-center">
+			<div className="tube bg-dark"></div>
+			<div className="base flex-column gap-3 rounded d-flex align-items-center p-4 bg-dark">
+				<div className={`light bg-danger ${lightUp == "danger" && "lightOn"}`} onClick={() => { handleClick("danger") }}></div>
+				<div className={`light bg-warning ${lightUp == "warning" && "lightOn"}`} onClick={() => { handleClick("warning") }}></div>
+				<div className={`light bg-success ${lightUp == "success" && "lightOn"}`} onClick={() => { handleClick("success") }}></div>
+			</div>
+			<div className="btn btn-dark mt-5 fs-2" onClick={() => {
+				if (lightUp == "danger") { setLightUp("warning") }
+				else if (lightUp == "warning") { setLightUp("success") }
+				else if (lightUp == "success") { setLightUp("danger") }
+			}}>Cambiar color</div>
 		</div>
 	);
 };
